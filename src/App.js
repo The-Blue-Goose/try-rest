26744a130ee1f00312ef30fa2400677c76adb991
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-09-30 14:25:36 -0400

    json

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () =>{
     const getJoke = () => {
         Axios.get("https://icanhazdadjoke.com/", {
         headers: {
-            "Accept": "text/plain",
+            "Accept": "application/json",
           }
         }
         ).then(
