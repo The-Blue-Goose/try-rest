0d3e83c6e572c6241edb3dd3680a67da66dd70d1
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-09-30 14:06:22 -0400

    dadjoke

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import './App.css';
-
-
+import Axios from 'axios';
 
 const App = () =>{
-
+    const getJoke = () => {
+        Axios.get("https://icanhazdadjoke.com/"
+        ).then(
+            (response) => {
+                console.log(response);
+                }
+            );
+    };
  return (
-
- <div className="container">
-
- <p>Mr. William H. Verhelle III</p>
-
- </div>
-
+<div className="container"><button onClick={getJoke}>dad joke</button>
+<br/><p>Hi!</p>
+</div>
  );
-
-}
-
-
-
+};
 export default App;
