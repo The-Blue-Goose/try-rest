8bc2cdda89d767817d1b0c53c6abc9da5e5022b3
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-09-30 14:23:23 -0400

    Json Joke

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import Axios from 'axios';
 
 const App = () =>{
     const getJoke = () => {
-        Axios.get("https://icanhazdadjoke.com/"
+        Axios.get("https://icanhazdadjoke.com/", {
+        headers: {
+            "Accept": "application/json",
+          }
+        }
         ).then(
             (response) => {
                 console.log(response);
                 }
             );
-    };
+        };
  return (
 <div className="container"><button onClick={getJoke}>dad joke</button>
 <br/><p>Hi!</p>
